724cbe3179e4192fe27197ad4d90f05e189bef0a
Tylerz3936 <tylerzhao103@gmail.com>
2024-07-21 10:38:03 -0400

    changed handlebar logic/ formatting structure, added seed for user and post, added a route

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const dbRoutes = require('./dbRoutes');
+
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/dashboard', dbRoutes);
 
 router.use((req, res) => {
   res.status(404).end();

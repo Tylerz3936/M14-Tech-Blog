5c595ae02a4e2118f1b3936a7ce5d1c4d1a812fb
Tylerz3936 <tylerzhao103@gmail.com>
2024-07-21 20:12:17 -0400

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-    port: 5432
-  }
-);
+let sequelize;
 
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres'
+    }
+  );
+}
 
 module.exports = sequelize;
